8808391a5fe8d10f8658ec99065c5aacf6e09302
surajit50 <surajit.shil.shil@gmail.com>
2023-10-03 07:35:37 +0530

    fixup

--- action/uploadAction.ts
+++ action/uploadAction.ts
@@ -1,122 +1,18 @@
-"use server";
+const isProduction = process.env.NODE_ENV === "production";
+const serverUrl = isProduction
+  ? process.env.NEXT_PUBLIC_SERVER_URL
+  : "http://localhost:3000";
 
-import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
-import { tmpdir } from "os";
-import { writeFile, unlink } from "fs/promises";
-import { v4 as uuidv4 } from "uuid";
-import path from "path";
-import { revalidatePath } from "next/cache";
-interface CloudinaryConfig {
-  cloud_name: string;
-  api_key: string;
-  api_secret: string;
-}
-
-const cloudinaryConfig: CloudinaryConfig = {
-  cloud_name: process.env.CLOUDINARY_NAME || "",
-  api_key: process.env.CLOUDINARY_KEY || "",
-  api_secret: process.env.CLOUDINARY_SECRET || "",
+export const uploadImage = async (imagePath: string) => {
+  try {
+    const response = await fetch(`${serverUrl}/api/upload`, {
+      method: "POST",
+      body: JSON.stringify({
+        path: imagePath,
+      }),
+    });
+    return response.json();
+  } catch (err) {
+    throw err;
+  }
 };
-
-cloudinary.config(cloudinaryConfig);
-
-interface SavedFile {
-  filepath: string;
-  filename: string;
-}
-
-async function savePhotoToLocal(formData: FormData): Promise<SavedFile[]> {
-  try {
-    const files: any = formData.getAll("files");
-    const tempdir: string = tmpdir();
-
-    const promises = files.map(async (file: any) => {
-      try {
-        const data = await file.arrayBuffer();
-        if (!data || data.byteLength === 0) {
-          throw new Error("File is empty");
-        }
-
-        const name: string = uuidv4();
-        const ext: any = file.type.split("/")[1];
-        const uploadDir: string = path.join(tempdir, `${name}.${ext}`);
-
-        const buffer: Buffer = Buffer.from(data);
-
-        await writeFile(uploadDir, buffer);
-
-        return { filepath: uploadDir, filename: file.name };
-      } catch (error: any) {
-        console.error("Error saving file:", error);
-        throw new Error("Failed to save photo: " + error.message);
-      }
-    });
-
-    const results: SavedFile[] = await Promise.all(promises);
-    return results;
-  } catch (error: any) {
-    console.error("Error in savePhotoToLocal:", error);
-    throw new Error("Failed to save photo: " + error.message);
-  }
-}
-
-async function uploadPhotoToCloudinary(
-  newFiles: SavedFile[]
-): Promise<{ success: boolean; result?: UploadApiResponse; error?: string }[]> {
-  try {
-    const uploadPromises: Promise<{
-      success: boolean;
-      result?: UploadApiResponse;
-      error?: string;
-    }>[] = newFiles.map(async (file: SavedFile) => {
-      try {
-        const result = await cloudinary.uploader.upload(file.filepath, {
-          folder: "imagegallery",
-        });
-        console.log(result);
-        return { success: true, result };
-      } catch (error: any) {
-        console.error("Error uploading file:", error.message);
-        console.error("Server Status Code:", error.status); // Log the status code
-        console.error("Server Response Headers:", error.headers); // Log the headers
-        console.error("Server Response Body:", error.response); // Log the response body
-        return { success: false, error: error.message };
-      }
-    });
-
-    const results: {
-      success: boolean;
-      result?: UploadApiResponse;
-      error?: string;
-    }[] = await Promise.all(uploadPromises);
-    return results;
-  } catch (error: any) {
-    console.error("Error in uploadPhotoToCloudinary:", error.message);
-    throw new Error("Failed to upload photo to Cloudinary: " + error.message);
-  }
-}
-
-export async function uploadPhoto(
-  formData: FormData
-): Promise<{ msg?: string; errMsg?: string }> {
-  try {
-    const newFiles: SavedFile[] = await savePhotoToLocal(formData);
-    await uploadPhotoToCloudinary(newFiles);
-
-    // Cleanup temporary files even in case of an error during Cloudinary upload
-    for (const file of newFiles) {
-      try {
-        await unlink(file.filepath);
-      } catch (unlinkError: any) {
-        console.error("Error deleting file:", unlinkError.message);
-      }
-    }
-
-    // Revalidate the path if needed
-    revalidatePath("/");
-    return { msg: "Upload Success" };
-  } catch (error: any) {
-    console.error("Error in uploadPhoto:", error.message);
-    return { errMsg: error.message };
-  }
-}
--- components/UploadForm.tsx
+++ components/UploadForm.tsx
@@ -1,66 +1,96 @@
 "use client";
-import { useRef, useState } from "react";
+
 import PhotoCard from "./PhotoCard";
 import ButtonSubmit from "./ButtonSubmit";
-import { uploadPhoto } from "@/action/uploadAction";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { uploadImage } from "@action/uploadAction";
 
 const UploadForm = () => {
-  const formRef = useRef<HTMLFormElement>(null);
-  const [files, setfiles] = useState<File[]>([]);
-  const [error, seterror] = useState("");
-  const handleInputFiles = async (e: any) => {
-    const files = e.target.files;
-    const newFiles = [...files].filter((file) => {
-      if (file.size < 1024 * 1024 * 1024 && file.type.startsWith("image/")) {
-        return file;
-      }
-    });
-    setfiles((prev) => [...newFiles, ...prev]);
-    formRef.current?.reset();
+  const [image, setimage] = useState("");
+  const router = useRouter();
+
+  const [submitting, setSubmitting] = useState<boolean>(false);
+
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+
+    if (!file.type.includes("image")) {
+      alert("Please upload an image!");
+
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      const result = reader.result as string;
+      setimage(result);
+    };
   };
+  const handleFormSubmit = async (e: FormEvent) => {
+    e.preventDefault();
 
-  async function handleDeleteFiles(index: any) {
-    const newFiles = files.filter((_, i) => i !== index);
-    setfiles(newFiles);
-  }
+    setSubmitting(true);
 
-  async function handleUploadfile() {
-    if (!files.length) return alert("No image files are selected");
-    if (files.length > 3) return alert("Upload upto 3 image files");
-    const formData = new FormData();
+    const imagepath = image;
 
-    files.forEach((file: File) => {
-      formData.append("files", file);
-    });
-    const res = await uploadPhoto(formData);
-    if (res?.errMsg) alert(`Error: ${res?.errMsg}`);
-  }
-
+    try {
+      const imageUrl = await uploadImage(imagepath);
+      console.log(imageUrl);
+      alert("image upload sucefully");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
   return (
-    <form action={handleUploadfile} ref={formRef}>
+    <form onSubmit={handleFormSubmit}>
       <div className="bg-gray-300 min-h-[200px] my-3 p-3">
-        <p className="text-red-500 font-bold">{error}</p>
-        <input type="file" accept="image/*" onChange={handleInputFiles} />
+        <label htmlFor="poster" className="flexCenter form_image-label"></label>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => handleChangeImage(e)}
+          multiple
+        />
 
         <h5 className="text-red-600">
-          (*) Only accept image files less than 1mb in size. up to 3 photo files
+          (*) Only accept image files less than 1MB in size. Up to 3 photo
+          files.
         </h5>
-
-        {/* Preview Image */}
-        <div className="flex  flex-wrap gap-3 my-3 ">
+      </div>
+      <button
+        type="submit"
+        className="bg-blue-600 py-1 px-3 text-white rounded ml-2"
+      >
+        Upload
+      </button>
+      <div></div>
+      {/* Preview Image 
+        <div className="flex flex-wrap gap-3 my-3">
           {files.map((file, index) => (
-            <PhotoCard
-              key={index}
-              url={URL.createObjectURL(file)}
-              onClick={() => handleDeleteFiles(index)}
-            />
+            <div key={index}>
+              <img
+                src={URL.createObjectURL(file)}
+                alt={`Preview ${file.name}`}
+              />
+              <button type="button" onClick={() => handleDeleteFile(index)}>
+                Delete
+              </button>
+            </div>
           ))}
         </div>
       </div>
-      <ButtonSubmit
-        value="Upload"
-        className="bg-blue-600 py-1 px-3 text-white rounded ml-2"
-      />
+      */}
     </form>
   );
 };
